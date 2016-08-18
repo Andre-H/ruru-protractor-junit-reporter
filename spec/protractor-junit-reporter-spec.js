992fcf2ffc24da4550fab341d90ebcbf9b4f2382
hulsea <andre.hulse@vizexplorer.com>
2016-08-18 20:01:06 +1200

    Removed target dir to debug build on CI

--- spec/protractor-junit-reporter-spec.js
+++ spec/protractor-junit-reporter-spec.js
@@ -9,37 +9,37 @@ describe('Generate JUnit XML Report', function () {
     it('should create XML from JSON', function (done) {
         var xmlReporter = new jSonXMLReporter({
             title : 'Protractor End to End Test Results',
-            xmlReportDestPath : './target/protractor-e2e-report-basic.xml'
+            xmlReportDestPath : 'protractor-e2e-report-basic.xml'
         });
         xmlReporter.generateXMLReport('./resources/protractor-e2e-report-basic.json');
-        expect(fs.readFileSync('./target/protractor-e2e-report-basic.xml')).toEqualFileContents('protractor-e2e-report-basic', done);
+        expect(fs.readFileSync('protractor-e2e-report-basic.xml')).toEqualFileContents('protractor-e2e-report-basic', done);
     });
 
     it('should be able to handle failed tests from JSON', function (done) {
         var xmlReporter = new jSonXMLReporter({
             title : 'Protractor End to End Test Results',
-            xmlReportDestPath : './target/protractor-e2e-report-with-failures.xml'
+            xmlReportDestPath : 'protractor-e2e-report-with-failures.xml'
         });
         xmlReporter.generateXMLReport('./resources/protractor-e2e-report-with-failures.json');
-        expect(fs.readFileSync('./target/protractor-e2e-report-with-failures.xml')).toEqualFileContents('protractor-e2e-report-with-failures',done);
+        expect(fs.readFileSync('protractor-e2e-report-with-failures.xml')).toEqualFileContents('protractor-e2e-report-with-failures',done);
     });
 
     it('should treat passed tests with the duration of 0 as skipped', function (done) {
         var xmlReporter = new jSonXMLReporter({
             title : 'Protractor End to End Test Results',
-            xmlReportDestPath : './target/protractor-e2e-report-with-skips.xml'
+            xmlReportDestPath : 'protractor-e2e-report-with-skips.xml'
         });
         xmlReporter.generateXMLReport('./resources/protractor-e2e-report-with-skips.json');
-        expect(fs.readFileSync('./target/protractor-e2e-report-with-skips.xml')).toEqualFileContents('protractor-e2e-report-with-skips',done);
+        expect(fs.readFileSync('protractor-e2e-report-with-skips.xml')).toEqualFileContents('protractor-e2e-report-with-skips',done);
     });
 
     it('should be able to handle a run without assertions', function (done) {
         var xmlReporter = new jSonXMLReporter({
             title : 'Protractor End to End Test Results',
-            xmlReportDestPath : './target/protractor-e2e-report-without-assertion.xml'
+            xmlReportDestPath : 'protractor-e2e-report-without-assertion.xml'
         });
         xmlReporter.generateXMLReport('./resources/protractor-e2e-report-without-assertion.json');
-        expect(fs.readFileSync('./target/protractor-e2e-report-without-assertion.xml')).toEqualFileContents('protractor-e2e-report-without-assertion',done);
+        expect(fs.readFileSync('protractor-e2e-report-without-assertion.xml')).toEqualFileContents('protractor-e2e-report-without-assertion',done);
     });
 
 });
