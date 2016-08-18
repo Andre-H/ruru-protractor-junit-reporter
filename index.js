9d8c9169a648d55a3b2598f6aa85e6ed7414dc77
hulsea <andre.hulse@vizexplorer.com>
2016-08-18 20:50:21 +1200

    Using moment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var XMLWriter = require('xml-writer');
+var moment = require('moment');
 
 var hasOwnProperty = Object.prototype.hasOwnProperty;
 
@@ -95,7 +96,7 @@ function XMLReporter(options) {
 		xw.writeAttribute('tests', jsonstr.length);
 		xw.writeAttribute('name', automationHeader);
 		xw.writeAttribute('time', totalTime);
-		xw.writeAttribute('timestamp', new Date().toISOString());
+		xw.writeAttribute('timestamp', moment().format());//new Date().toISOString());
 
 		for (var t = 0; t < testArray.length; t++) {
 			xw.startElement('testcase');
